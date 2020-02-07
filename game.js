80c360763c9796e8e265c6d50a0d376f1fbee861
vancerweston <vweston@student.neumont.edu>
2020-02-07 16:20:58 -0700

    added main.css. Updated game.js and index.html

--- game.js
+++ game.js
@@ -1,16 +1,28 @@
 let myNum = Math.round(Math.random() *99) + 1;
     console.log(myNum);
+let count = 0;
     
 function sendData(form) {
     let guessNum = document.forms['myform']['inputbox'].value;
     let text;
+    count++;
+    let background = document.body;
+    let winner = document.getElementById('hidden');
 
     if (guessNum == myNum) {
-        text = 'You win!';
+        text = 'You win! It took you ' + count + ' tries to get it right!';
+        background.style.backgroundColor = 'grey';
+        winner.style.display = 'flex';
+    } else if (guessNum > 100 || guessNum < 1 || isNaN(guessNum)) {
+        text = 'Invaild. Please play again, and enter a vaild number.';
+    } else if (guessNum > myNum) {
+        text = 'Too high! Try again!';
+    } else if (guessNum < myNum) {
+        text = 'Too low! Try again!';
     } else {
-        text = 'I\'m sorry, try again.';
+        text = 'Something went wrong in the game. Please reload the page and play again.';
     }
-    document.getElementById('response').innerHTML = text;
+    document.getElementById('theiranswer').innerHTML = text;
 }
 
 
